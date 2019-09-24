4bf3b018fbe5c1895c701d013225726223048949
alekszavialov <avelhar74@gmail.com>
2019-09-24 23:19:04 +0300

    as

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+
+const initialState = {
+  books: [
+    {
+      id: 1,
+      title: 'Production-Ready Microservices',
+      author: 'Susab J. Fowler'
+    },
+    {
+      id: 2,
+      title: 'Release It!',
+      author: 'Michael T. Nygard'
+    }
+  ]
+};
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'BOOKS_LOADED':
+      return {
+        books: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
